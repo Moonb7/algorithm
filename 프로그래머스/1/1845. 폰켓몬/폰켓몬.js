5caa7450863cd7457737b615905492da8792c042
Jinsu Mun <133336225+Moonb7@users.noreply.github.com>
2024-08-29 20:26:30 +0900

    [level 1] Title: 폰켓몬, Time: 6.43 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,13 +1,21 @@
 function solution(nums) {
-    const mypoketCount = Math.floor(nums.length / 2);
-    const ponketmon = new Set;
+    // 해시 테이블 역할을 하는 객체 생성
+    let pokemonCount = {};
     
-    for(let i = 0; i < nums.length; i++) {
-        if(ponketmon.size < mypoketCount) {
-            ponketmon.add(nums[i]);
+    // 폰켓몬 종류별로 개수를 세기
+    for (let num of nums) {
+        if (pokemonCount[num]) {
+            pokemonCount[num] += 1;
+        } else {
+            pokemonCount[num] = 1;
         }
-        else break;
     }
     
-    return ponketmon.size;
+    // 고를 수 있는 폰켓몬의 최대 종류 수
+    const maxTypes = Object.keys(pokemonCount).length;
+    // 실제로 선택할 수 있는 폰켓몬의 수
+    const maxPokemon = nums.length / 2;
+    
+    // 최대 가능한 종류와 실제 선택 가능한 수 중 작은 값을 반환
+    return Math.min(maxTypes, maxPokemon);
 }
