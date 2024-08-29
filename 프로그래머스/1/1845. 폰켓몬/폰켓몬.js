9c827bd306febdab169cfee8f89f3ee7a66aed91
Jinsu Mun <133336225+Moonb7@users.noreply.github.com>
2024-08-29 20:11:06 +0900

    [level 1] Title: 폰켓몬, Time: 1.16 ms, Memory: 34.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,14 @@
+function solution(nums) {
+    let answer = 0;
+    const mypoketCount = Math.floor(nums.length / 2);
+    const ponketmon = new Set;
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(ponketmon.size < mypoketCount) {
+            ponketmon.add(nums[i]);
+        }
+        else break;
+    }
+    
+    return ponketmon.size;
+}
