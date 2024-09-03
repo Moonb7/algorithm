97141693b04b7ff370c45f1b54888df9fe38d80b
Jinsu Mun <133336225+Moonb7@users.noreply.github.com>
2024-09-03 12:21:35 +0900

    [level 1] Title: 문자열 나누기, Time: 26.26 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -7,8 +7,6 @@ function solution(s) {
         
         const currentChar = stack.filter(char => char == stack[0]); // 처음들어온 문자의 갯수를 구하기위해 
         const compareChar = stack.filter(char => char !== stack[0]); // 처음들어온 문자가 아닌 다음에 들어온 문자의 수를 정하기 위해
-        
-        
         if(currentChar.length == compareChar.length) {
             answer++;
             stack.length = 0; // 초기화하여 다시 반복할 준비를 합니다.
