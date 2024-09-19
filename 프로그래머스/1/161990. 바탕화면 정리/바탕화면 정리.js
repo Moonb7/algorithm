253057e17e6d5b6a45072c00c824c0d52d796927
Jinsu Mun <133336225+Moonb7@users.noreply.github.com>
2024-09-20 00:12:54 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.35 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,29 @@
+function solution(wallpaper) {
+    let answer = [];
+    // 시작점은 원소들을 확인할때 처음으로 (#)이 있는 열을 세로, 가로는 원소중 먼저 가로길이 x위치 파일(#)이 있는 위치
+    // 끝점은 끝에 열 (#)이 있으면 끝점 세로는 이 열이고, 그리고 가로는 원소중 가장 긴 가로길이 max위치 파일(#)이 있는 위치
+    let findind = new Set();
+    
+    for(let i = 0; i < wallpaper.length; i++) {
+        const paperArr = wallpaper[i].split("");
+        // #이 있는 인덱스 확인 그리고 그중에서 가장 작은 수는 시작점의 가로, 가장 큰수는 끝점의 가로
+        if(paperArr.includes("#")) {
+            findind.add(paperArr.indexOf("#"));
+            findind.add(paperArr.lastIndexOf("#"))
+        }
+        
+        // 첫 시작점의 세로열 확인
+        if(wallpaper[i].includes("#") && !answer[0] && answer[0] !== 0) {
+            answer[0] = i;
+        }
+        
+        // 끝 점의 세로
+        if(wallpaper[wallpaper.length - i - 1].includes("#") && !answer[2]) {
+            answer[2] = wallpaper.length - i;
+        }
+    }
+    answer[1] = Math.min(...findind);
+    answer[3] = Math.max(...findind) + 1;
+    
+    return answer;
+}
