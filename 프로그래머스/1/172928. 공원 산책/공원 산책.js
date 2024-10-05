5ec27862b20c2e556ed7aadbab2ad4901a8153ed
Jinsu Mun <133336225+Moonb7@users.noreply.github.com>
2024-10-05 18:56:05 +0900

    [level 1] Title: 공원 산책, Time: 0.73 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -0,0 +1,37 @@
+function solution(park, routes) {
+    let answer = [];
+    const parkArea = [park.length, park[0].length];
+    const directions = {
+        "E": [0, 1],
+        "W": [0, -1],
+        "S": [1, 0],
+        "N": [-1, 0],
+    }
+    
+    // 시작위치 구하기
+    for(let i = 0; i < park.length; i++) {
+        if(park[i].includes("S")) {
+            const idx = park[i].split("").findIndex((element) => {return element === "S"});
+            answer = [i, idx]
+            break;
+        }
+    }
+    
+    // 위치 이동
+    for(let i = 0; i < routes.length; i++) {
+        const [dir, moveLength] = routes[i].split(" ");
+        let [h, w] = answer;
+        for(let j = 0; j < moveLength; j++) {
+            h += directions[dir][0];
+            w += directions[dir][1];
+            if(h < 0 || w < 0 || h >= parkArea[0] || w >= parkArea[1] || park[h][w] === "X") {
+                [h, w] = answer; // 초기화
+                // console.log(h, w, park[h][w]);
+                break;
+            }
+        }
+        answer = [h, w];
+    }
+    
+    return answer;
+}
