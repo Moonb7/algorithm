0f7ce54e1cf1196214cd2763f4f57edbf3c8f983
Jinsu Mun <133336225+Moonb7@users.noreply.github.com>
2024-10-17 09:41:04 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,15 +1,5 @@
 function solution(s) {
-    const strArray = s.split(" ");
-    
-    let answer = '';
-    
-    let min = null;
-    let max = null;
-    for(let i = 0; i < strArray.length; i++) {
-        if(Number(strArray[i]) < min || min === null) min = Number(strArray[i]);
-        if(Number(strArray[i]) > max || max === null) max = Number(strArray[i]);
-    }
-    answer = `${min} ${max}`
-    
-    return answer;
+    const arr = s.split(' ');
+
+    return Math.min(...arr)+' '+Math.max(...arr);
 }
