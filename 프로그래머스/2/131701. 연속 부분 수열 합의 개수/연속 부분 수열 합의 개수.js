ec2d9c55348a324c1ac7ea80287c0758aeb09866
Jinsu Mun <133336225+Moonb7@users.noreply.github.com>
2024-12-31 19:53:10 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 2419.23 ms, Memory: 57.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,19 @@
+function solution(elements) {
+    const answer = new Set();
+    
+    // 수열 길이가 1에서 elements.length까지 순회
+    for(let i = 1; i <= elements.length; i++) {
+        for(let j = 0; j < elements.length; j++) {
+            let sum = 0;
+            // i 만큼 해당 요소 더해주기
+            for(let k = 0; k < i; k++) {
+                //console.log("요소------------ : ",elements[(j + k) % elements.length])
+                sum += elements[(j + k) % elements.length];
+            }
+            //console.log("sum : ", sum)
+            
+            answer.add(sum); // 집합에 추가
+        }
+    }
+    return answer.size;
+}
