bed56c1cb21d15410150ccde2a3348e27f83668a
Jinsu Mun <133336225+Moonb7@users.noreply.github.com>
2024-12-31 19:54:12 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 105.18 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,19 +1,12 @@
 function solution(elements) {
-    const answer = new Set();
-    
-    // 수열 길이가 1에서 elements.length까지 순회
-    for(let i = 1; i <= elements.length; i++) {
-        for(let j = 0; j < elements.length; j++) {
-            let sum = 0;
-            // i 만큼 해당 요소 더해주기
-            for(let k = 0; k < i; k++) {
-                //console.log("요소------------ : ",elements[(j + k) % elements.length])
-                sum += elements[(j + k) % elements.length];
-            }
-            //console.log("sum : ", sum)
-            
-            answer.add(sum); // 집합에 추가
+    const circular = elements.concat(elements);
+    const set = new Set();
+    for (let i = 0; i < elements.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < elements.length; j++) {
+            sum += circular[i + j];
+            set.add(sum);
         }
     }
-    return answer.size;
+    return set.size;
 }
